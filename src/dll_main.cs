0428a176100859c35acb79f56b56cb66eebc64d8
Winsent <wnsentonegog@gmail.com>
2026-09-15 20:14:29 +0300

    Add project files.

---
+++ src/dll_main.cs
@@ -0,0 +1,16 @@
+﻿global using static korn;
+
+namespace Zodiak;
+
+public unsafe class dll_main
+{
+    [EntryPoint]
+    public static bool DllMain(nint hinstDLL, uint fdwReason, nint lpvReserved)
+    {
+        if (fdwReason == 1)
+        {
+            
+        }
+        return true;
+    }
+}
